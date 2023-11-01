d7b346ab083d65756c6fbb634a494b9c230f4c1c
Thoratsahil <stsahil1717@gmail.com>
2023-11-01 16:32:17 +0530

    commit

---
+++ FindVowels.java
@@ -0,0 +1,21 @@
+class FindVowels
+{
+	public static void main(String args[])
+	{
+		String s=" Virat Kohli";
+		int count=0;
+		for(int i=0;i<s.length();i++)
+		{
+			char c=s.charAt(i);
+		if(c=='A' || c=='a' || c=='E' || c=='e' ||c=='I' || c=='i' ||c=='O' || c=='o' ||c=='U' || c=='u')
+		    count++;
+		  	
+		
+		}
+		System.out.print("count vowels = "+count);
+		
+	}
+
+
+	
+}
