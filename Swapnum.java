60cebc0574dd3baf2833c8d5d5d8dd8de0d30463
Thoratsahil <stsahil1717@gmail.com>
2023-11-01 16:41:55 +0530

    commit

---
+++ Swapnum.java
@@ -0,0 +1,18 @@
+class Swapnum
+{
+  public static void main(String args[])
+  {
+	  int a=22;
+	  int b=33;
+	  int temp;
+	  System.out.println(a);
+	  System.out.println(b);
+	  
+	   temp=a;
+	   a=b;
+	   b=temp;
+	  
+	  System.out.println("After"+a);
+	  System.out.println("After"+b);
+  }
+}
