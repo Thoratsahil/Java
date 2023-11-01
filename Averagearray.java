f0455be00b2f8255069b99813d65ac08e64ed140
Thoratsahil <stsahil1717@gmail.com>
2023-11-01 17:03:12 +0530

    commit

---
+++ Averagearray.java
@@ -0,0 +1,19 @@
+class Averagearray
+{
+	public static void main(String args[])
+    {
+		int a[]={10,10,10,10,10,10};
+		 int sum=0;
+		 int avg=0;
+		 
+		for(int i=0;i<a.length;i++)
+			sum=a[i]+sum;
+			System.out.println("array Addition "+ sum);
+		
+		System.out.println();
+		
+		avg=sum/6 ;
+		System.out.println(avg);
+		
+	}	
+}
