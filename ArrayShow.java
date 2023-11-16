6f97d628d9a4d67f7ecbc1cde29490df731cb9b1
Thoratsahil <stsahil1717@gmail.com>
2023-11-16 22:41:56 +0530

    Commit

---
+++ ArrayShow.java
@@ -0,0 +1,12 @@
+class Arrshow
+{
+   public static void main(String args[])
+      {
+           Object a[]={"ACV",11,33.3,'s'};
+           for(int i=0;i<=a.length-1;i++)
+              {
+                System.out.println(a[i]);
+              }
+        
+      }
+}
